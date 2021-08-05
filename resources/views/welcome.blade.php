edab21ed25db27ef4f5ec5b85d488715a5c9e580
ognjen91 <qzman16@gmail.com>
2021-08-05 12:31:47 +0200

    added homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,10 @@
 <x-layout-booker>
-    {{-- <date-picker></date-picker> --}}
-    <h1>home</h1>
+
+    <h1 class='text-center red--text'>Booking Mamma <br> <span class="blue--text">Brodovi Petar</span></h1>
+    <h2 class='my-2 text-center'><a href="/nova">Admin</a></h2>
+    <v-divider></v-divider>
+    <h2 class='my-2 text-center'><a href="{{route('booker.homepage')}}">Booker</a></h2>
+    <v-divider></v-divider>
+    <h2 class='my-2 text-center'><a href="{{route('crew.homepage')}}">Posada</a></h2>
+
 </x-layout-booker>
