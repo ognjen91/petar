b3e5813f4eeccb9066621bfa9d7fe00789e095ac
ognjen91 <qzman16@gmail.com>
2021-08-14 14:58:19 +0200

    fixed alert sms text

--- resources/views/emails/admin/excursion-almost-full.blade.php
+++ resources/views/emails/admin/excursion-almost-full.blade.php
@@ -1,6 +1,6 @@
 @component('mail::message')
 # Upozorenje: Izlet je skoro popunjen
-## izlet: {{$excursion->name}}
+## izlet: {{$excursion->name}} {{\Carbon\Carbon::parse($excursion->departure)->format('d.m.Y.')}}
 ### Ukupno mjesta: {{$excursion->total_seats}}
 ### Trenutno slobodno mjesta: {{$excursion->freeSeats}}
 
